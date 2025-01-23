58829341c001226eab4f7186b219314a58a1b46b
Kunt Betilmerzaev <kunt.betilmerzaev@mail.ru>
2025-01-23 12:55:23 +0300

    rea

---
+++ 8kyu/Return-Negative.js
@@ -0,0 +1,12 @@
+// Description:
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Solution
+
+function makeNegative(num) {
+    if (num > 0) {
+        console.log(-num)
+    } else {
+        console.log(num)
+    }
+}
